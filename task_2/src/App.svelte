4e72eb9e1ca965246b61885a4b3dae9a7773e7ba
Никита Милютин <nikmilhero@yandex.ru>
2024-04-20 19:40:20 +0700

    Выполнено второе задание

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,60 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+     import { onMount } from "svelte";
+     let currencies: string[] = ["USD", "EUR", "GBP", "CNY"];
+     let fromCurrency: string = "USD";
+     let toCurrency: string = "EUR";
+     let fromAmount: number = 1;
+     let toAmount: number = 0;
+     let rate: number = 0;
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+     const apiKey: string = "0554a88af41d55ca749cbd22";
 
-  <div class="card">
-    <Counter />
-  </div>
+     async function fetchRate(): Promise<void> {
+       const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`);
+       const data = await response.json();
+       rate = data.conversion_rates[toCurrency];
+       convert("from");
+     }
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+     function convert(direction: string): void {
+       if (direction === "from") {
+         toAmount = parseFloat((fromAmount * rate).toFixed(2));
+       } else {
+         fromAmount = parseFloat((toAmount / rate).toFixed(2));
+       }
+     }
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+     onMount(() => {
+       fetchRate();
+     });
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+     $: fromCurrency, toCurrency, fetchRate();
+     $: fromAmount, convert("from");
+     $: toAmount, convert("to");
+   </script>
+
+   <style>
+     input {
+       margin: 5px 0;
+     }
+   </style>
+
+   <div>
+     <h1>Конвертер валют</h1>
+     <div>
+      <select bind:value={fromCurrency}>
+       {#each currencies as currency}
+         <option>{currency}</option>
+       {/each}
+      </select>
+     <input type="number" bind:value={fromAmount} />
+     </div>
+     <div>
+      <select bind:value={toCurrency}>
+       {#each currencies as currency}
+         <option>{currency}</option>
+       {/each}
+     </select>
+     <input type="number" bind:value={toAmount} />
+    </div>
+   </div>
